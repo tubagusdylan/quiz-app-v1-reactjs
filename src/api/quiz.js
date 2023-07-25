405ba55e0e9c76d8f65a3502d3f8a4c9ef7ab244
tubagusdylan <tubagus.dylanr@gmail.com>
2023-07-25 20:27:17 +0700

    sudah bisa consume api

--- src/api/quiz.js
+++ src/api/quiz.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseUrl = import.meta.env.VITE_BASEURL;
 
-export async function getQuizList() {
-  const { data } = await axios.get(`${baseUrl}`);
+export async function getQuizList(category, total, diff, type) {
+  const { data } = await axios.get(`${baseUrl}?amount=${total}&category=${category}&difficulty=${diff}&type=${type}`);
   return data.results;
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,15 @@ import { Link } from "react-router-dom";
 import "./Home.css";
 import { useState } from "react";
 import { questions } from "../api/dataQuestion";
+import { getQuizList } from "../api/quiz";
+import { user } from "../api/dataUser";
 
 export function Home() {
-  const [category, setCategory] = useState(9);
+  const [category, setCategory] = useState(0);
   const [totalQuestions, setTotalQuesitons] = useState(5);
   const [difficulty, setDifficulty] = useState("");
   const [type, setType] = useState("");
+  const [username, setUsername] = useState("");
 
   function handleCategory(e) {
     setCategory(e.target.value);
@@ -26,15 +29,27 @@ export function Home() {
     setType(e.target.value);
   }
 
+  async function handleStart(category, totalQuestions, difficulty, type) {
+    const result = await getQuizList(category, totalQuestions, difficulty, type);
+    questions.push(...result);
+    user.name = username;
+    setUsername("");
+    setTotalQuesitons(5);
+    setCategory(9);
+    setDifficulty("");
+    setType("");
+    console.log(questions);
+  }
+
   return (
     <div className="background">
       <h1>Quiz App</h1>
       <div className="container-home">
-        <form>
+        <form onSubmit={(e) => e.preventDefault()}>
           <label htmlFor="name" className="label">
             Username
           </label>
-          <input type="text" id="name" name="name" className="input-name" />
+          <input type="text" id="name" name="name" className="input-name" required onChange={({ target }) => setUsername(target.value)} value={username} />
           <label htmlFor="total-questions" className="label">
             Number of Questions
           </label>
@@ -49,16 +64,16 @@ export function Home() {
           </label>
           <select name="category" id="category" className="select" value={category} onChange={handleCategory}>
             <option value="">Choose Category</option>
-            <option value="9">General Knowledge</option>
-            <option value="10">Entertainment: Books</option>
-            <option value="11">Entertainment: Film</option>
-            <option value="12">Entertainment: Music</option>
-            <option value="13">Entertainment: Musicals & Theatres</option>
-            <option value="14">Entertainment: Television</option>
-            <option value="15">Entertainment: Video Games</option>
-            <option value="16">Entertainment: Board Games</option>
-            <option value="17">Science Nature</option>
-            <option value="18">Science: Computers</option>
+            <option value={9}>General Knowledge</option>
+            <option value={10}>Entertainment: Books</option>
+            <option value={11}>Entertainment: Film</option>
+            <option value={12}>Entertainment: Music</option>
+            <option value={13}>Entertainment: Musicals & Theatres</option>
+            <option value={14}>Entertainment: Television</option>
+            <option value={15}>Entertainment: Video Games</option>
+            <option value={16}>Entertainment: Board Games</option>
+            <option value={17}>Science Nature</option>
+            <option value={18}>Science: Computers</option>
           </select>
           <label htmlFor="difficulty" className="label">
             Difficulty
@@ -77,8 +92,10 @@ export function Home() {
             <option value="multiple">Multiple choice</option>
             <option value="boolean">True / False</option>
           </select>
-          <Link to="/play">
-            <button className="button-home">Start</button>
+          <Link to="/play" className="link">
+            <button className="button-home" onClick={() => handleStart(category, totalQuestions, difficulty, type)}>
+              Start
+            </button>
           </Link>
         </form>
       </div>
